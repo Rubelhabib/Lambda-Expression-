a577c55bc12db9bd1649312ac032dc44020bb567
Rubelhabib <rubelhabib007@gmail.com>
2024-02-13 10:58:40 +0600

    Add project files.

---
+++ Lambda Expression)/Anonymous method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression_
+{
+    internal class Anonymous_method
+    {
+        public static string EmpName(string fristName)
+        {
+            return " Hello " + fristName + " have a good day";
+        }
+    }
+}
---
+++ Lambda Expression)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression_
+{
+    public delegate string delName(string fristName);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            delName intsDel = (scendName) =>
+            {
+                return " Hello " + scendName + " have a good day";
+
+            };
+            string str = intsDel.Invoke("Rubel_Habib");
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+        }
+    }
+}
